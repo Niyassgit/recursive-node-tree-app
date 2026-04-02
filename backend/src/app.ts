89e3e803e716845b675aaa2a345a5a1e15a004f1
Niyas Nazar <niyas28nazar@gmail.com>
2026-04-02 23:10:31 +0530

    folder struture has been created

---
+++ backend/src/app.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Test Route
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+export default app;
